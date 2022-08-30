0e390b804ad036afa091ab8615eae67a1dfec7e5
hend <jimanhakim92@gmail.com>
2022-08-30 07:46:26 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,57 @@
+function add(num1, num2) {
+    let res = num1 + num2
+    return res
+}
+
+function subtract(num1, num2) {
+    let res = num1 - num2
+    return res
+}
+
+function multiply(num1, num2) {
+    let res = num1 * num2
+    return res
+}
+
+function divide(num1, num2) {
+    let res = num1 / num2
+    return res
+}
+
+function add(a, b) {
+    let res = a + b
+    return res
+}
+
+function subtract(a, b) {
+    let res = a - b
+    return res
+}
+
+function multiply(a, b) {
+    let res = a * b
+    return res
+}
+
+function divide(a, b) {
+    let res = a / b
+    return res
+}
+
+const increment = function (n) {
+    return ++n
+}
+
+const decrement = function (n) {
+    return --n
+}
+
+const makeInt = function (n) {
+    parseInt(n, 10);
+    return parseInt(n, 10)
+}
+const preserveDecimal = function (n) {
+    parseFloat(n);
+    return parseFloat(n)
+}
+
